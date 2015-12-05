76be636523477877d5b5986dbb224999722e0604
Sukonnik Illia <illia.sukonnik@gmail.com>
2015-12-05 02:20:18 +0200

    Short count-neighbours.py

---
+++ count-neighbours.py
@@ -0,0 +1,26 @@
+def count_neighbours(grid, row, col):
+    return sum(sum(i[max(col-1, 0):col+2])
+               for i in
+               grid[max(row-1, 0):row+2]) - \
+        grid[row][col]
+
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 1, 2) == 3, "1st example"
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example"
+    assert count_neighbours(((1, 1, 1),
+                             (1, 1, 1),
+                             (1, 1, 1),), 0, 2) == 3, "Dense corner"
+    assert count_neighbours(((0, 0, 0),
+                             (0, 1, 0),
+                             (0, 0, 0),), 1, 1) == 0, "Single"
