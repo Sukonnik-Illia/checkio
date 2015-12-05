2458b52a3b15418d3195a14f7c16f905122098bb
Sukonnik Illia <illia.sukonnik@gmail.com>
2015-12-06 00:51:37 +0200

    most wanted letter, dissatisfied with the decision

---
+++ most-wanted-letter.py
@@ -0,0 +1,18 @@
+def checkio(text):
+    chars = [i for i in text.lower() if i.isalpha()]
+    d = {i: chars.count(i) for i in set(chars)}
+    m = max([d[i] for i in d])
+    d = [i for i in d if d[i] == m]
+    return sorted(d)[0]
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio("Hello World!") == "l", "Hello test"
+    assert checkio("How do you do?") == "o", "O is most wanted"
+    assert checkio("One") == "e", "All letter only once."
+    assert checkio("Oops!") == "o", "Don't forget about lower case."
+    assert checkio("AAaooo!!!!") == "a", "Only letters."
+    assert checkio("abe") == "a", "The First."
+    print("Start the long test")
+    assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
+    print("The local tests are done.")
