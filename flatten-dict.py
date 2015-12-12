e763afc7b965ad8fd4bdac30e83599b218155301
Sukonnik Illia <illia.sukonnik@gmail.com>
2015-12-12 16:06:49 +0200

    fix bug in flatten-dict.py

--- flatten-dict.py
+++ flatten-dict.py
@@ -1,12 +1,38 @@
-def rec(path, value, result):
-    if type(value) == dict and value != {}:
-        for key in value:
-            rec(path + [key], value[key], result)
-    else:
-        result['/'.join(path)] = value if value != {} else ""
+import pdb
 
-
-def flatten(d):
+def flatten(dictionary):
+    stack = [((), dictionary)]
     result = {}
-    rec([], d, result)
+    while stack:
+        path, current = stack.pop()
+        for k, v in current.items():
+            if isinstance(v, dict) and v != {}:
+                stack.append((path + (k,), v))
+            else:
+                result["/".join((path + (k,)))] = v if v != {} else ""
     return result
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert flatten({"key": "value"}) == {"key": "value"}, "Simple"
+    assert flatten(
+        {"key": {"deeper": {"more": {"enough": "value"}}}}
+    ) == {"key/deeper/more/enough": "value"}, "Nested"
+    assert flatten({"empty": {}}) == {"empty": ""}, "Empty value"
+    assert flatten({"name": {
+                        "first": "One",
+                        "last": "Drone"},
+                    "job": "scout",
+                    "recent": {},
+                    "additional": {
+                        "place": {
+                            "zone": "1",
+                            "cell": "2"}}}
+    ) == {"name/first": "One",
+          "name/last": "Drone",
+          "job": "scout",
+          "recent": "",
+          "additional/place/zone": "1",
+          "additional/place/cell": "2"}
+    print('done')
