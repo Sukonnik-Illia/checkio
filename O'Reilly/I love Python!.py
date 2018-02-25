4a6c81ae316ffa8cfa6a35445958208248d8e80a
Illia Sukonnik <Illia Sukonnik>
2018-02-25 22:39:09 +0200

    O'Reilly:I love Python!

---
+++ O'Reilly/I love Python!.py
@@ -0,0 +1,6 @@
+def i_love_python():
+    return "I love Python!" or "I don't like creativity just for creativity"
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert i_love_python() == "I love Python!"
