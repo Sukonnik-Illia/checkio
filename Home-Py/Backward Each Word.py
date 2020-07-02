bae08b4447087921e9514a3de4d1529f7694e6e4
Illia Sukonnik <illia.sukonnik@gmail.com>
2020-07-02 12:44:02 +0300

    Home-Py/Backward Each Word

---
+++ Home-Py/Backward Each Word.py
@@ -0,0 +1,19 @@
+import re
+
+
+def backward_string_by_word(text: str) -> str:
+    # your code here
+    return ''.join(i[::-1] for i in re.findall('\w+|\ +', text))
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(backward_string_by_word(''))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert backward_string_by_word('') == ''
+    assert backward_string_by_word('world') == 'dlrow'
+    assert backward_string_by_word('hello world') == 'olleh dlrow'
+    assert backward_string_by_word('hello   world') == 'olleh   dlrow'
+    assert backward_string_by_word('welcome to a game') == 'emoclew ot a emag'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
